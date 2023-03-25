6c2c8e2957dd69faa472c1aa02e311ad13d17d2f
WIDZpy <raphstrug@gmail.com>
2023-03-25 20:56:53 +0100

    test tween pc portable

--- test_jeux/AnimationPlayer.gd
+++ test_jeux/AnimationPlayer.gd
@@ -10,5 +10,21 @@ func _ready():
 func _process(delta):
 	pass
 
-func prepar(pos, scal, angle):
-	 
+func adapt(scale, target_scale, angle, target_angle):
+	var animation = Animation.new()
+	var track_1 = animation.add_track(Animation.TYPE_VALUE)
+	var lib = AnimationLibrary.new()
+	animation.track_set_path(track_1, ":scale")
+	animation.track_insert_key(track_1, 0.0, 0.5)
+	
+	animation.track_insert_key(track_1, 1.0, 1)
+	
+	animation.track_insert_key(track_1, 2.0, 0.5)
+	
+	lib.add_animation("anim", animation)
+
+	self.add_animation_library("lib", lib)
+	
+	
+	
+	
--- test_jeux/bouton_anime2.gd
+++ test_jeux/bouton_anime2.gd
@@ -1,33 +1,24 @@
 extends TextureButton
 var screen_size
-var i = 0
-signal tap
+var tw = create_tween()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	screen_size = get_viewport_rect().size
 	rotation = 0
 	pivot_offset = (size/2)
 	position = (screen_size - size) / 2
-	$AnimationPlayer.adapt(scale, scale+scale/4, rotation, rotation+5)
-	
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
 
 func _on_button_down():
-	tap.emit()
-	i += 1
-	if $AnimationPlayer.is_playing():
-		$AnimationPlayer.stop()
-	
-	
-	$AnimationPlayer.play("lib/anim")
-	print($AnimationPlayer.has_animation("lib/anim"))
-		
-	
-
+	print()
+	if tw.is_running() == null:
+		print("fe")
+	tw.stop()
+	tw.tween_property(self, "scale", Vector2(), 1)	
+	tw.tween_property(self, "scale", Vector2(1, 1), 1)
